fd80114fb50b946ce6b19951279c0eedb4c6d9fe
dominic <dominicseduraja3@gmail.com>
2023-04-22 07:35:47 +0530

    validation commit

--- Form-Validation/script.js
+++ Form-Validation/script.js
@@ -34,6 +34,42 @@ function getElementById(input){
     return input.id.CharAt(0).toUpperCase() + input.id.slice(1);
 }
 
+// check input length
 function checkLength(input,min,max){
-    
+    if(input.value.length < min){
+        showError(input,`${getFieldName(input)} must be at least ${min} characters`);
+    }else if(input.value.length > max){
+        showError(input,`${getFieldName(input)} must be less then ${max} characters`)
+    }else{
+        showSuccess(input)
+    }
 }
+
+// check email is valid
+function checkEmail(input){
+     const re = "<>"
+    // /^(([^<>()[]\.,;:s@"]+(.[^<>()[]\.,;:s@"]+)*)|
+    // (".+"))@(([[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}])|(([a-
+    // zA-Z-0-9]+.)+[a-zA-Z]{2,}))$/;
+    if(re.test(input.value.trim())){
+        showSuccess(input);
+    }else{
+        showError(input, "E-Mail is Not Valid")
+    }
+}
+
+// check password match
+function checkPasswordMatch(input1,input2){
+    if(input1.value !== input2.value){
+        showError(input2, "Password do not match")
+    }
+}
+
+form.addEventListener("submit", (e)=>{
+    e.preventDefault();
+    checkRequired([username, email, password, password, CPassword]);
+    checkLength(username, 3, 15);
+    checkLength(password , 8, 25);
+    checkEmail(email);
+    checkPasswordMatch(password, CPassword)
+});
